f7fbf4f1ac952c84b0efed534f7066bdf1ea5991
joel rivera <jorivera114@gmail.com>
2023-08-24 18:27:37 -0400

    feat: slop-12

--- frontend/src/components/tab-panels.jsx
+++ frontend/src/components/tab-panels.jsx
@@ -1,13 +1,10 @@
 import { Tab } from "@headlessui/react";
 
-export const TabPanels = ({
-  children = "content 1",
-  child = "content 434",
-}) => {
+export const TabPanels = ({ children = "content 1" }) => {
   return (
     <Tab.Panels>
       <Tab.Panel>{children}</Tab.Panel>
-      <Tab.Panel>{child}</Tab.Panel>
+      <Tab.Panel></Tab.Panel>
     </Tab.Panels>
   );
 };
