379a7a5ad927419d0ca5a761258b9bd2d5fb4421
Greg Cunningham <greg@mycunningham.com>
2023-09-08 08:35:13 -0500

    feat: button-component

---
+++ frontend/src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+export default function Button({ title, className }) {
+  return <button className={className}>{title} </button>;
+}
+
+
+
+
+
---
+++ frontend/src/components/Button/Button.stories.jsx
@@ -0,0 +1,43 @@
+import Button from "./Button";
+import { action } from '@storybook/addon-actions';
+
+export default {
+  title: "Component/Button",
+  component: Button,
+};
+
+const Template = (args) => <Button {...args} />;
+
+
+export const Primary = Template.bind({});
+Primary.args = {
+  title: 'Primary',
+  className: 'bg-yellow-200 h-12 text-white w-80 border py-4 px-4',
+   onClick: {
+    action: 'clicked',
+}
+}
+export const Secondary = Template.bind({});
+Secondary.args = {
+  title: 'Secondary Button',
+  className: 'bg-yellow-200 w-96 h-12 text-white w-80 border py-4 px-4',
+  onClick: {
+    action: 'clicked',
+}
+}  
+export const Large = Template.bind({});
+Large.args = {
+  title: 'Large Button',
+  className: 'bg-transparent h-10 text-white text-lg w-36 h-10 p-2.border py-4 px-4 ',
+  onClick: {
+    action: 'clicked',
+}
+}
+export const Small = Template.bind({});
+Small.args = {
+  title: 'Small Button',
+  className: 'bg-yellow-200 h-10 text-white text-lg w-36 h-10 p-2.5',
+  onClick: {
+    action: 'clicked',
+}
+}
