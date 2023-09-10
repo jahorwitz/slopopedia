5c5e963f461aa6fc00f19536fa000f2060786997
Josh Horwitz <jahorwitz94@gmail.com>
2023-09-09 20:35:01 -0400

    fix: removed unnecessary dependency

--- frontend/src/components/Button/Button.stories.jsx
+++ frontend/src/components/Button/Button.stories.jsx
@@ -1,42 +1,42 @@
-import Button from './Button'
+import Button from "./Button";
 
 export default {
-  title: 'Component/Button',
-  component: Button
-}
+  title: "Component/Button",
+  component: Button,
+};
 
-const Template = args => <Button {...args} />
+const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({})
+export const Primary = Template.bind({});
 Primary.args = {
-  title: 'Primary',
-  className: 'bg-yellow-200 h-12 text-white w-80 border py-4 px-4',
+  title: "Primary",
+  className: "bg-yellow-200 h-12 text-white w-80 border py-4 px-4",
   onClick: {
-    action: 'clicked'
-  }
-}
-export const Secondary = Template.bind({})
+    action: "clicked",
+  },
+};
+export const Secondary = Template.bind({});
 Secondary.args = {
-  title: 'Secondary Button',
-  className: 'bg-yellow-200 w-96 h-12 text-white w-80 border py-4 px-4',
+  title: "Secondary Button",
+  className: "bg-yellow-200 w-96 h-12 text-white w-80 border py-4 px-4",
   onClick: {
-    action: 'clicked'
-  }
-}
-export const Large = Template.bind({})
+    action: "clicked",
+  },
+};
+export const Large = Template.bind({});
 Large.args = {
-  title: 'Large Button',
+  title: "Large Button",
   className:
-    'bg-transparent h-10 text-white text-lg w-36 h-10 p-2.border py-4 px-4 ',
+    "bg-transparent h-10 text-white text-lg w-36 h-10 p-2.border py-4 px-4 ",
   onClick: {
-    action: 'clicked'
-  }
-}
-export const Small = Template.bind({})
+    action: "clicked",
+  },
+};
+export const Small = Template.bind({});
 Small.args = {
-  title: 'Small Button',
-  className: 'bg-yellow-200 h-10 text-white text-lg w-36 h-10 p-2.5',
+  title: "Small Button",
+  className: "bg-yellow-200 h-10 text-white text-lg w-36 h-10 p-2.5",
   onClick: {
-    action: 'clicked'
-  }
-}
+    action: "clicked",
+  },
+};
