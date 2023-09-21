6e168d29dd7abda575b0118d908d501a25deef11
austinp <auspendleton@gmail.com>
2023-09-21 17:19:47 -0600

    feat: reusable form component

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,3 +1,5 @@
+import { Button } from "../components/Button/Button";
+
 export const Form = ({ className, children, onSubmit, ...rest }) => {
   return (
     <form className={className} onSubmit={onSubmit} {...rest}>
@@ -6,18 +8,20 @@ export const Form = ({ className, children, onSubmit, ...rest }) => {
   );
 };
 
-Form.TextInput = ({ className, id, email, password, ...rest }) => {
+Form.TextInput = ({ className, labelText, id, email, password, ...rest }) => {
   return (
     <>
-      <div className={cx("flex flex-col", className)}>
+      <div className="flex font-bold font-arial flex-col">
         <label htmlFor={id} className="">
           {labelText}
         </label>
+      </div>
+      <div className="flex flex-col border-solid rounded-none border-black-500">
         <input
           id={id}
-          className=""
+          className="flex font-normal font-arial flex-col border-solid rounded-none border-black-500"
           type="text"
-          placeholder="Type here"
+          placeholder="Type"
           {...rest}
         />
       </div>
@@ -28,12 +32,57 @@ Form.TextInput = ({ className, id, email, password, ...rest }) => {
 Form.TextArea = ({ className, labelText, id, register, ...rest }) => {
   return (
     <>
-      <div className={cx("flex flex-col", className)}>
+      <div className="flex font-bold font-arial flex-col border-solid rounded-none border-black-500">
         <label htmlFor={id} className="">
           {labelText}
         </label>
-        <textarea id={id} className="" type="text" {...rest} />
+        <textarea
+          id={id}
+          className="font-normal"
+          type="text"
+          placeholder="Type"
+          {...rest}
+        />
       </div>
     </>
   );
+};
+Form.TextNumber = ({ className, labelText, id, email, password, ...rest }) => {
+  return (
+    <>
+      <div className="flex font-bold font-arial flex-col border-solid rounded-none border-black-500">
+        <label htmlFor={id} className="">
+          {labelText}
+        </label>
+        <input
+          id={id}
+          className="font-normal"
+          type="number"
+          placeholder="Type"
+          {...rest}
+        />
+      </div>
+    </>
+  );
+};
+Form.Dropdown = ({ className, labelText, id, email, password, ...rest }) => {
+  return (
+    <>
+      <div className="flex font-bold font-arial flex-col border-solid rounded-none border-black-500">
+        <label htmlFor={id} className="">
+          {labelText}
+        </label>
+        <select
+          id={id}
+          className=""
+          type="dropdown"
+          placeholder="Dropdown"
+          {...rest}
+        />
+      </div>
+    </>
+  );
+};
+Form.Submit = ({ children }) => {
+  return <Button type="submit">{children}</Button>;
 };
--- src/components/form.stories.jsx
+++ src/components/form.stories.jsx
@@ -1,3 +1,27 @@
+// import { Form } from "./form";
+
+// export default {
+//   title: "Components/Form",
+//   component: Form,
+// };
+
+// const Template = (args) => <Form {...args} />;
+
+// export const TextInput = Template.bind({});
+// TextInput.args = {
+//   className: "h-20 text-black font-arialRegular rounded-lg",
+//   id: "Title",
+//   labelText: "Title",
+//   placeholder: "Type",
+// };
+
+// export const TextArea = Template.bind({});
+// TextArea.args = {
+//   className: "h-20 text-black font-arialRegular rounded-lg",
+//   id: "Title",
+//   labelText: "Description",
+//   placeholder: "Type",
+// };
 import { Form } from "./form";
 
 export default {
@@ -5,20 +29,14 @@ export default {
   component: Form,
 };
 
-const Template = (args) => <Form {...args} />;
+export const Default = () => (
+  <Form>
+    <Form.TextInput className="" id="username" labelText="Title" />
 
-export const TextInput = Template.bind({});
-TextInput.args = {
-  className: "h-20 text-black font-arialRegular rounded-lg",
-  id: "Title",
-  labelText: "Title",
-  placeholder: "Type",
-};
+    <Form.TextArea className="" id="comment" labelText="Description" />
 
-export const TextArea = Template.bind({});
-TextArea.args = {
-  className: "h-20 text-black font-arialRegular rounded-lg",
-  id: "Title",
-  labelText: "Description",
-  placeholder: "Type",
-};
+    <Form.TextNumber className="" id="number" labelText="Number" />
+    <Form.Dropdown className="" id="dropdown" labelText="Dropdown" />
+    <Form.Submit>Yeah!</Form.Submit>
+  </Form>
+);
