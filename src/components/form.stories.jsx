ba927964a0e65c0533b0405953f7b6ee24f93a9e
austinp <auspendleton@gmail.com>
2023-10-07 12:37:42 -0600

    feat: reusable form component - hook added

--- src/components/form.stories.jsx
+++ src/components/form.stories.jsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import { Form } from "./form";
 
 export default {
@@ -5,14 +6,38 @@ export default {
   component: Form,
 };
 
-export const Default = () => (
-  <Form className={"w-full max-w-lg mx-auto p-4 bg-white"}>
-    <Form.TextInput className="" id="username" labelText="Title" />
+export const Default = () => {
+  const { register, handleSubmit } = useForm();
 
-    <Form.TextArea className="" id="comment" labelText="Description" />
+  return (
+    <Form className={"w-full max-w-lg mx-auto p-4 bg-white"}>
+      <Form.TextInput
+        className=""
+        id="username"
+        labelText="Title"
+        register={register}
+      />
 
-    <Form.TextNumber className="" id="number" labelText="Number" />
-    <Form.Dropdown className="" id="dropdown" labelText="Dropdown" />
-    <Form.Submit className="" title="" />
-  </Form>
-);
+      <Form.TextArea
+        className=""
+        id="comment"
+        labelText="Description"
+        register={register}
+      />
+
+      <Form.TextNumber
+        className=""
+        id="number"
+        labelText="Number"
+        register={register}
+      />
+      <Form.Dropdown
+        className=""
+        id="dropdown"
+        labelText="Dropdown"
+        register={register}
+      />
+      <Form.Submit className="" title="" handleSubmit={handleSubmit} />
+    </Form>
+  );
+};
