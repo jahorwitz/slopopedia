4c59b109c4f853f5e287a6e5c6a26e3298b43a9e
Brittney Alfieri <vbrit95@hotmail.com>
2023-10-13 10:58:04 -0400

    feat(layout): feature/slop-10/layout-submit-slop

---
+++ src/components/SubmitList.jsx
@@ -0,0 +1,26 @@
+import { Header } from "./index";
+import { Link } from "react-router-dom";
+
+export const SubmitList = () => {
+  return (
+    <>
+      <Header>
+        <Header.Logo />
+        <Header.NavLinks />
+        <Header.Profile />
+      </Header>
+      <div className="max-w-[1440px] mx-auto">
+        <div className="flex flex-col justify-center items-center relative ">
+          <h1 className="mb-10 mt-10 xs:self-end xs:pl-2 xs:pr-2  ">//Add PageTitle component here</h1>
+          <Link to={"/submit"} className="absolute left-10 xs:left-4 sm:left-4 xs:text-sm top-[40px] border-b-2 border-black">
+            Back to submit page
+          </Link>
+          <div className="max-w-[453px] h-[728px] ">
+            //Add the submitted slops Component Here
+          </div>
+          <div className="mb-10 mt-24">//add footer component here//</div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,6 @@
 export * from "./Header";
 export * from "./Modal";
 export * from "./protected-route";
+export * from "./Submit";
+export * from "./SubmitList";
+export * from "../components/Button/Button";
