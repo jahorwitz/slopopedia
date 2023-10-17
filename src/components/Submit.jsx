1229beb5920f959b5cff47ffc486ddbc940434ac
Josh Horwitz <josh.horwitz@floqast.com>
2023-10-17 19:11:16 -0400

    fix: linting

--- src/components/Submit.jsx
+++ src/components/Submit.jsx
@@ -41,24 +41,24 @@ const Form = () => {
 const submitForm = () => {
   return (
     <div className="max-w-[1440px] mx-auto ">
-        <div className="flex flex-col justify-center items-center xs:px-5 sm:px-5">
-          <h1 className="mb-40 mt-10">//Add PageTitle component here</h1>
-          <p className="max-w-[627px] xs:text-sm sm:text-center md:text-center lg:text-center font-arialBold">
-            Thanks for submitting a slop to our platform, dear goblin!
-          </p>
-          <p className="max-w-[632px] xs:text-sm font-arialBold ">
-            Our team of professional slop goblins will review your submission
-            and publish it, if your slop is actually sloppy, and doesn't repeat
-            movies already published here.
-          </p>
-          <div className="mt-40 xs:mt-16 ">
-            <Button
-              className="w-[400px] h-10 bg-yellow-button border text-lg font-arialBold xs:text-sm xs:w-[285px]"
-              title="Submit another one?"
-            ></Button>
-          </div>
-          <div className="mb-10 mt-40">//add footer component here//</div>
+      <div className="flex flex-col justify-center items-center xs:px-5 sm:px-5">
+        <h1 className="mb-40 mt-10">//Add PageTitle component here</h1>
+        <p className="max-w-[627px] xs:text-sm sm:text-center md:text-center lg:text-center font-arialBold">
+          Thanks for submitting a slop to our platform, dear goblin!
+        </p>
+        <p className="max-w-[632px] xs:text-sm font-arialBold ">
+          Our team of professional slop goblins will review your submission and
+          publish it, if your slop is actually sloppy, and doesn't repeat movies
+          already published here.
+        </p>
+        <div className="mt-40 xs:mt-16 ">
+          <Button
+            className="w-[400px] h-10 bg-yellow-button border text-lg font-arialBold xs:text-sm xs:w-[285px]"
+            title="Submit another one?"
+          ></Button>
         </div>
+        <div className="mb-10 mt-40">//add footer component here//</div>
       </div>
+    </div>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
 // Ex:
 // export * from './my-component'
+export * from "../components/Button/Button";
 export * from "./Header";
 export * from "./Modal";
 export * from "./protected-route";
 export * from "./Submit";
 export * from "./SubmitList";
-export * from "../components/Button/Button";
