d26001f19d20841db20b5a798d7032d7c5b47511
Francis Flanagan <7flanaganf@gmail.com>
2023-12-06 14:54:53 +0900

    feat: finish FestHeader

--- src/routes/fest-route/fest-header.jsx
+++ src/routes/fest-route/fest-header.jsx
@@ -1,37 +1,38 @@
-const fest = {
-  name: "Gobb Fest",
-  attendees: [
-    {
-      name: "frank",
-      id: "",
-    },
-    {
-      name: "frank2",
-      id: "",
-    },
-    {
-      name: "frank3",
-      id: "",
-    },
-    {
-      name: "frank4",
-      id: "",
-    },
-  ],
-  startDate: "2023-11-25",
-  endDate: "2023-11-30",
-};
+import { Button, Keyword } from "../../components";
 
-export const FestHeader = () => {
+import checkMark from "../../images/check-mark.svg";
+
+export const FestHeader = ({ fest }) => {
+  let { name, attendees, startDate, endDate } = fest;
+  startDate = new Intl.DateTimeFormat("en-US").format(new Date(startDate));
+  endDate = new Intl.DateTimeFormat("en-US").format(new Date(endDate));
+
   return (
-    <div>
-      <div>
-        <h1>{fest.name}</h1>
-        {/* map of array of attendees */}
+    <div className="flex flex-col gap-y-4 py-10">
+      <div className="flex justify-between items-center">
+        <h1 className="font-arial scale-y-2 font-bold">{name.toUpperCase()}</h1>
+        <Button
+          variant="secondary"
+          size="sm"
+          className="flex items-center gap-x-2.5 pb-3"
+        >
+          <img src={checkMark} className="w-4 h-3" alt="white check mark" />
+          I'm going!
+        </Button>
       </div>
-      <div>
-        {/* going button */}
-        {/* start and end dates */}
+      <div className="flex justify-between items-start">
+        <div className="flex flex-wrap gap-2.5 max-w-lg">
+          {attendees.map((attendee, index) => (
+            <Keyword
+              className="bg-gray"
+              key={index}
+              keyword={attendee.username}
+            />
+          ))}
+        </div>
+        <p className="font-arial text-lg/4 opacity-60">
+          {startDate} ~ {endDate}
+        </p>
       </div>
     </div>
   );
--- src/routes/fest-route/fest-route.jsx
+++ src/routes/fest-route/fest-route.jsx
@@ -1,12 +1,15 @@
 import { useQuery } from "@apollo/client";
+import { useState } from "react";
 import { useLocation, useParams } from "react-router";
 import { Header } from "../../components";
 import { GET_FEST } from "../../graphql/get-fest";
+import { FestHeader } from "./fest-header";
 import { FestSidebar } from "./fest-sidebar";
 
 export const FestRoute = () => {
   const { festId } = useParams();
   const location = useLocation();
+  const [fest, setFest] = useState({});
   const { data, loading, error } = useQuery(GET_FEST, {
     variables: {
       where: {
@@ -14,8 +17,7 @@ export const FestRoute = () => {
       },
     },
   });
-  console.log(location);
-  // console.log(data);
+
   return (
     <>
       <Header>
@@ -23,8 +25,10 @@ export const FestRoute = () => {
         <Header.NavLinks />
         <Header.Profile />
       </Header>
-      <FestSidebar />
-      {/* fest name, going button, attendees, start and end dates */}
+      <div className=" max-w-[1200px] my-0 mx-auto box-border">
+        {!loading && <FestHeader fest={data.fest} />}
+        <FestSidebar />
+      </div>
     </>
   );
 };
