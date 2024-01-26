4fdf169728978046114cd6a1279ea1733d7a876f
Dillona25 <Dillyb00by49@gmail.com>
2024-01-25 22:27:59 -0600

    feature/fixed-error-submit-pr-logout-logic

--- src/main.jsx
+++ src/main.jsx
@@ -25,11 +25,7 @@ import {
   SoundsRoute,
 } from "./routes";
 import { FestRoute } from "./routes/fest-route";
-import {
-  AuthProvider,
-  CurrentUserContextProvider,
-  ModalContextProvider,
-} from "./store";
+import { CurrentUserContextProvider, ModalContextProvider } from "./store";
 
 const httpLink = createHttpLink({
   uri:
@@ -62,83 +58,79 @@ const client = new ApolloClient({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ApolloProvider client={client}>
     <React.StrictMode>
-      <AuthProvider>
-        <CurrentUserContextProvider>
-          <ModalContextProvider>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<MainRoute />} />
-                <Route path="/browse" element={<BrowseRoute />} />
-                <Route path="/fests" element={<FestsRoute />} />
-                <Route path="/movie" element={<MovieRoute />} />
-                <Route path="/search" element={<SearchRoute />} />
-                <Route path="/sounds" element={<SoundsRoute />} />
-                <Route path="/blog" element={<BlogRoute />} />
-                <Route path="/draft" element={<DraftRoute />} />
-                <Route path="/article" element={<ArticleRoute />} />
-                <Route
-                  path="/profile"
-                  element={
-                    <ProtectedRoute user={"user"}>
-                      <ProfileRoute />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/profile/fests"
-                  element={
-                    <ProtectedRoute>
-                      <ProfileFestsRoute />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/preferences/:value"
-                  element={
-                    <ProtectedRoute>
-                      <PreferencesRoute />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/submit"
-                  element={
-                    <ProtectedRoute>
-                      <Submit />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/submit-list"
-                  element={
-                    <ProtectedRoute>
-                      <SubmitList />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/recommend"
-                  element={
-                    <ProtectedRoute>{/* <Recommend /> */}</ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/blog"
-                  element={<ProtectedRoute>{/* <Blog /> */}</ProtectedRoute>}
-                />
-                <Route
-                  path="/fests/:festId"
-                  element={
-                    <ProtectedRoute>
-                      <FestRoute />
-                    </ProtectedRoute>
-                  }
-                />
-              </Routes>
-            </BrowserRouter>
-          </ModalContextProvider>
-        </CurrentUserContextProvider>
-      </AuthProvider>
+      <CurrentUserContextProvider>
+        <ModalContextProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<MainRoute />} />
+              <Route path="/browse" element={<BrowseRoute />} />
+              <Route path="/fests" element={<FestsRoute />} />
+              <Route path="/movie" element={<MovieRoute />} />
+              <Route path="/search" element={<SearchRoute />} />
+              <Route path="/sounds" element={<SoundsRoute />} />
+              <Route path="/blog" element={<BlogRoute />} />
+              <Route path="/draft" element={<DraftRoute />} />
+              <Route path="/article" element={<ArticleRoute />} />
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute user={"user"}>
+                    <ProfileRoute />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/profile/fests"
+                element={
+                  <ProtectedRoute>
+                    <ProfileFestsRoute />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/preferences/:value"
+                element={
+                  <ProtectedRoute>
+                    <PreferencesRoute />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/submit"
+                element={
+                  <ProtectedRoute>
+                    <Submit />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/submit-list"
+                element={
+                  <ProtectedRoute>
+                    <SubmitList />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/recommend"
+                element={<ProtectedRoute>{/* <Recommend /> */}</ProtectedRoute>}
+              />
+              <Route
+                path="/blog"
+                element={<ProtectedRoute>{/* <Blog /> */}</ProtectedRoute>}
+              />
+              <Route
+                path="/fests/:festId"
+                element={
+                  <ProtectedRoute>
+                    <FestRoute />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+        </ModalContextProvider>
+      </CurrentUserContextProvider>
     </React.StrictMode>
   </ApolloProvider>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,3 @@
-export * from "./auth-context-provider";
 export * from "./current-user-context-provider";
 export * from "./modal-context-provider";
 export * from "./use-modals";
