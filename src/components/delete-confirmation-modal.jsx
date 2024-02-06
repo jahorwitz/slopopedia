1d6b9d5ae8058132c65922a75a714479677787b6
mnunezsa <mnunezsa95@gmail.com>
2024-02-06 07:18:45 -0500

    fix / change the 'cancel' button from rounded to sharp edges and remove the y-scale property from the <p> element in the DeleteConfirmationModal based on Francis' recommendations on pull request

--- src/components/delete-confirmation-modal.jsx
+++ src/components/delete-confirmation-modal.jsx
@@ -7,7 +7,7 @@ export function DeleteConfirmationModal({ confirmButtonAction }) {
   return (
     <>
       <Modal title="Are you sure you want to delete?">
-        <p className="text-sm scale-y-2 font-arialRegular text-center pt-6 pb-2">
+        <p className="text-xl font-arialRegular text-center pt-6 pb-2">
           There is no way to undo this action...
         </p>
         <div className="flex flex-row justify-center mt-6 mb-10">
@@ -20,7 +20,7 @@ export function DeleteConfirmationModal({ confirmButtonAction }) {
           </button>
           <button
             type="button"
-            className="text-dark font-arialBold mr-2 bg-yellow p-1 rounded-md"
+            className="text-dark font-arialBold mr-2 bg-yellow p-1 "
             onClick={() => closeModal("")}
           >
             Cancel
