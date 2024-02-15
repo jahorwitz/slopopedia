960b51959aa7cea2935522852e8a16dad0d8373a
Danielle Foss <danifoss2011@gmail.com>
2024-02-15 13:06:16 -0700

    fix: connect to local db for testing

--- src/main.jsx
+++ src/main.jsx
@@ -32,7 +32,8 @@ const httpLink = createHttpLink({
   uri:
     import.meta.env.MODE === "production"
       ? import.meta.env.VITE_API_URI
-      : "https://slopopedia-api-a5fe9aef64e8.herokuapp.com/api/graphql",
+      : "http://localhost:8080/api/graphql",
+  //: "https://slopopedia-api-a5fe9aef64e8.herokuapp.com/api/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -51,7 +52,8 @@ const client = new ApolloClient({
     import.meta.env.MODE === "production"
       ? //created environment variables must be prefixed by VITE
         import.meta.env.VITE_API_URI
-      : "https://slopopedia-api-a5fe9aef64e8.herokuapp.com/api/graphql",
+      : "http://localhost:8080/api/graphql",
+  // : "https://slopopedia-api-a5fe9aef64e8.herokuapp.com/api/graphql",
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
