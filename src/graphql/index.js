75b92161007695656e822f93661164a1f001fc90
Danielle Foss <danifoss2011@gmail.com>
2024-02-18 10:35:00 -0700

    feat: add get-fests to index

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,6 +1,7 @@
 export * from "./create-user";
 export * from "./delete-fest";
 export * from "./get-fest";
+export * from "./get-fests";
 export * from "./get-movies";
 export * from "./get-user-authentication";
 export * from "./get-user-fests";
