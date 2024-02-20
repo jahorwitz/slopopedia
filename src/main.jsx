986a7f1b4b8142cd605a27a3db05d22c5bffe27f
Francis Flanagan <7flanaganf@gmail.com>
2024-02-20 15:39:07 +0900

    refactor: merge main and deal with conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "../src/global/default.css";
+import { App } from "./App";
+import { ClientContextProvider } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
