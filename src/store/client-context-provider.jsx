c37880e573ece57456fce16a0fa12844cccb2174
Francis Flanagan <7flanaganf@gmail.com>
2024-02-29 09:14:13 +0900

    refactor: prevent client instance from being remade on login

--- src/store/client-context-provider.jsx
+++ src/store/client-context-provider.jsx
@@ -5,37 +5,65 @@ import { ClientContext } from "./client-context";
 
 export const ClientContextProvider = ({ children }) => {
   const [token, setToken] = useState(null);
+  const [client, setClient] = useState(null);
 
   useEffect(() => {
     setToken(localStorage.getItem("jwt"));
   }, []);
 
-  const httpLink = createHttpLink({
-    uri:
-      //in Vite, use special object `import.meta.env` to access enviroment variables
-      import.meta.env.MODE === "production"
-        ? //created environment variables must be prefixed by VITE
-          import.meta.env.VITE_API_URI
-        : "https://slopopedia-api-a5fe9aef64e8.herokuapp.com/api/graphql",
-  });
+  useEffect(() => {
+    const httpLink = createHttpLink({
+      uri:
+        //in Vite, use special object `import.meta.env` to access enviroment variables
+        import.meta.env.MODE === "production"
+          ? //created environment variables must be prefixed by VITE
+            import.meta.env.VITE_API_URI
+          : "https://slopopedia-api-a5fe9aef64e8.herokuapp.com/api/graphql",
+    });
 
-  const authLink = setContext((_, { headers }) => {
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    };
-  });
+    const authLink = setContext((_, { headers }) => {
+      return {
+        headers: {
+          ...headers,
+          authorization: token ? `Bearer ${token}` : "",
+        },
+      };
+    });
+    setClient(
+      new ApolloClient({
+        link: authLink.concat(httpLink),
+        cache: new InMemoryCache(),
+      })
+    );
+  }, []);
 
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
+  useEffect(() => {
+    if (client) {
+      const httpLink = createHttpLink({
+        uri:
+          //in Vite, use special object `import.meta.env` to access enviroment variables
+          import.meta.env.MODE === "production"
+            ? //created environment variables must be prefixed by VITE
+              import.meta.env.VITE_API_URI
+            : "https://slopopedia-api-a5fe9aef64e8.herokuapp.com/api/graphql",
+      });
 
-  return (
+      const authLink = setContext((_, { headers }) => {
+        return {
+          headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+          },
+        };
+      });
+
+      client.setLink(authLink.concat(httpLink));
+    }
+  }, [token, client]);
+
+  return client ? (
     <ClientContext.Provider value={{ token, setToken, client }}>
       {children}
     </ClientContext.Provider>
-  );
+  ) : null;
 };
