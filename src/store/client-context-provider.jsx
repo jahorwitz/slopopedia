a1b02b9bf44dda077c0f8e648423cb5b5670dc59
Brennan Duck <brennantduck@gmail.com>
2024-04-25 09:27:51 -0400

    fix: using ternary or to set uri based on node_env

--- src/store/client-context-provider.jsx
+++ src/store/client-context-provider.jsx
@@ -4,56 +4,56 @@ import { useEffect, useState } from "react";
 import { ClientContext } from "./client-context";
 
 export const ClientContextProvider = ({ children }) => {
-    const [token, setToken] = useState(localStorage.getItem("jwt"));
-    const [client, setClient] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("jwt"));
+  const [client, setClient] = useState(null);
 
-    const apiUri =
-        process.env.NODE_ENV === "production"
-            ? "https://slopopedia-api-a5fe9aef64e8.herokuapp.com/api/graphql"
-            : "http://localhost:8080/api/graphql";
+  const apiUri =
+    process.env.NODE_ENV === "production"
+      ? "https://slopopedia-api-a5fe9aef64e8.herokuapp.com/api/graphql"
+      : "http://localhost:8080/api/graphql";
 
-    useEffect(() => {
-        const httpLink = createHttpLink({
-            uri: apiUri,
-        });
+  useEffect(() => {
+    const httpLink = createHttpLink({
+      uri: apiUri,
+    });
 
-        const authLink = setContext((_, { headers }) => {
-            return {
-                headers: {
-                    ...headers,
-                    authorization: token ? `Bearer ${token}` : "",
-                },
-            };
-        });
-        setClient(
-            new ApolloClient({
-                link: authLink.concat(httpLink),
-                cache: new InMemoryCache(),
-            })
-        );
-    }, []);
+    const authLink = setContext((_, { headers }) => {
+      return {
+        headers: {
+          ...headers,
+          authorization: token ? `Bearer ${token}` : "",
+        },
+      };
+    });
+    setClient(
+      new ApolloClient({
+        link: authLink.concat(httpLink),
+        cache: new InMemoryCache(),
+      })
+    );
+  }, []);
 
-    useEffect(() => {
-        if (client) {
-            const httpLink = createHttpLink({
-                uri: apiUri,
-            });
+  useEffect(() => {
+    if (client) {
+      const httpLink = createHttpLink({
+        uri: apiUri,
+      });
 
-            const authLink = setContext((_, { headers }) => {
-                return {
-                    headers: {
-                        ...headers,
-                        authorization: token ? `Bearer ${token}` : "",
-                    },
-                };
-            });
-            client.setLink(authLink.concat(httpLink));
-        }
-    }, [token, client]);
+      const authLink = setContext((_, { headers }) => {
+        return {
+          headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+          },
+        };
+      });
+      client.setLink(authLink.concat(httpLink));
+    }
+  }, [token, client]);
 
-    return client ? (
-        <ClientContext.Provider value={{ token, setToken, client }}>
-            {children}
-        </ClientContext.Provider>
-    ) : null;
+  return client ? (
+    <ClientContext.Provider value={{ token, setToken, client }}>
+      {children}
+    </ClientContext.Provider>
+  ) : null;
 };
