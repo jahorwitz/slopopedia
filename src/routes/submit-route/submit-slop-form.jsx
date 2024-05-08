0df76853826e3e70718ae0870d824dff9d15edf4
ATribbey <alx_036@yahoo.com>
2024-05-08 17:26:58 -0500

    fix: make runtime and releasYear required; ensure they properly affect formState

--- src/routes/submit-route/submit-slop-form.jsx
+++ src/routes/submit-route/submit-slop-form.jsx
@@ -195,17 +195,19 @@ export const SubmitSlopForm = () => {
         <div className="xs:flex-col sm:flex md:flex lg:flex xl:flex justify-between font-arialBold text-lg box-border ">
           <div className="flex flex-col">
             <Form.TextInput
+              required
               maxLength={4}
               value={releaseYear}
               errors={errors}
               register={register("releaseYear", {
+                required: "Release Year is required",
                 minLength: {
                   value: 4,
                   message: "Please enter 4 digits",
                 },
               })}
               placeholder="Release Year"
-              labelText="Release Year"
+              labelText="Release Year *"
               onChange={(e) => {
                 if (!isNaN(e.target.value) && e.target.value !== " ") {
                   setValue("releaseYear", e.target.value, {
@@ -222,10 +224,15 @@ export const SubmitSlopForm = () => {
           </div>
           <div className="flex flex-col">
             <Form.TextInput
+              required
               maxLength={3}
               value={runtime}
+              errors={errors}
+              register={register("runtime", {
+                required: "Runtime is required",
+              })}
               placeholder="Runtime"
-              labelText="Runtime"
+              labelText="Runtime *"
               onChange={(e) => {
                 if (!isNaN(e.target.value) && e.target.value !== " ") {
                   setValue("runtime", e.target.value, { shouldValidate: true });
