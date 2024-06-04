320ad76d6bf4effb30d0e227740787a5deb6cf7a
Brennan Duck <brennantduck@gmail.com>
2024-06-04 09:23:20 -0400

    feat: refactored submit form. awaiting slop-63 to handle the image uploading

--- src/routes/submit-route/submit-slop-form.jsx
+++ src/routes/submit-route/submit-slop-form.jsx
@@ -1,74 +1,56 @@
 import { useMutation, useQuery } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-import { Form } from "../../components";
-import { CREATE_MOVIE, GET_MOVIES } from "../../graphql";
+import { Button } from "../../components/button/button";
+import Dropdown from "../../components/form/advanced-form-inputs/dropdown";
+import Dropzone from "../../components/form/advanced-form-inputs/dropzone";
+import FormError from "../../components/form/advanced-form-inputs/formError";
+import TextArea from "../../components/form/advanced-form-inputs/textArea";
+import TextInput from "../../components/form/advanced-form-inputs/textInput";
+import { CREATE_MOVIE } from "../../graphql";
 import { GET_KEYWORDS } from "../../graphql/get-keywords";
 import { useCurrentUser } from "../../hooks";
-import titleImage from "../../images/Submit a slop.png";
 
-export const SubmitSlopForm = () => {
-  const { currentUser } = useCurrentUser();
-  const [createMovie] = useMutation(CREATE_MOVIE);
-  const [submitted, setSubmitted] = useState(false);
-  const [movieKeywords, setMovieKeywords] = useState([]);
-  const [movieSubmitted, setMovieSubmitted] = useState(false);
-
-  // Grab the keywords from the database
-  const { data: keywordsData } = useQuery(GET_KEYWORDS);
-
-  // If we have keywords, then setMovieKeywords to the data in the response
-  useEffect(() => {
-    if (keywordsData) {
-      setMovieKeywords(keywordsData.keywords);
-    }
-  }, [keywordsData]);
-
-  // Reseting submitted data so we can submit another if the user wants
-  const resetSubmitted = () => {
-    setSubmitted(false);
-    setMovieSubmitted(true);
+export default function SubmitSlopForm() {
+  const defaultFormValues = {
+    title: "",
+    description: "",
+    releaseYear: "",
+    runtime: "",
+    howToWatch: "",
   };
-
-  // React hook form variables and default form values
   const {
     register,
-    watch,
     handleSubmit,
-    setValue,
-    getValues,
+    control,
     reset,
-    formState: { errors, isValid, isSubmitSuccessful },
+    formState: { errors, isValid },
   } = useForm({
-    defaultValues: {
-      title: "",
-      description: "",
-      releaseYear: "",
-      runtime: "",
-      tomatoScore: "",
-      howToWatch: "",
-      keywords: [],
-    },
+    defaultValues: defaultFormValues,
   });
 
-  // Gettting the form values with useForm
-  const {
-    title,
-    description,
-    releaseYear,
-    runtime,
-    tomatoScore,
-    howToWatch,
-    keywords,
-  } = getValues();
-
-  // Handle submit function. The constants are expected to be integers so we need to run parseInt to make the default string value and integer
-  const handleFormSubmit = async (data) => {
-    setSubmitted(true);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const { currentUser } = useCurrentUser();
+  const { data: keywordsData } = useQuery(GET_KEYWORDS);
+  const [createMovie] = useMutation(CREATE_MOVIE);
+  const keywords = [];
+  for (let i = 0; i < keywordsData?.keywords.length; i++) {
+    const obj = {};
+    obj["label"] = keywordsData?.keywords[i].name;
+    obj["value"] = keywordsData?.keywords[i].id;
+    keywords.push(obj);
+  }
+  function onSubmit(data) {
+    if (data.keywords.length <= 0) {
+      return alert("Please select at least one keyword.");
+    }
     const releaseYearInt = parseInt(data.releaseYear);
     const runTimeInt = parseInt(data.runtime);
     const tomatoScoreInt = parseInt(data.tomatoScore);
+    /*
+              photo stuff still needs to be handled
+              */
     try {
       createMovie({
         variables: {
@@ -78,268 +60,170 @@ export const SubmitSlopForm = () => {
                 id: currentUser.id,
               },
             },
-            title,
-            description,
+            title: data.title,
+            description: data.description,
             releaseYear: releaseYearInt,
             runtime: runTimeInt,
             tomatoScore: tomatoScoreInt,
-            howToWatch,
-            // photo: {
-            //   upload: data.photo
-            // },
+            howToWatch: data.howToWatch,
             keywords: {
-              connect: keywords.map((keyword) => ({
-                id: keyword.id,
+              connect: data.keywords.map((keyword) => ({
+                id: keyword,
               })),
             },
           },
         },
       });
-      console.log("Data sent");
-      hasSubmittedMovie();
-    } catch (err) {
-      console.error("Error in createMovies request", err);
+      reset({});
+      setIsSubmitted(true);
+    } catch (e) {
+      console.error("Error in request", e);
     }
-  };
+  }
 
-  // useEffect that resets our values when the form is submitted
-  useEffect(() => {
-    if (isSubmitSuccessful) {
-      reset({
-        title: "",
-        description: "",
-        releaseYear: "",
-        runtime: "",
-        tomatoScore: "",
-        howToWatch: "",
-        keywords: "",
-      });
-    }
-  }, [isSubmitSuccessful]);
-
-  // Querying movies and adding the needed, variables. Setting the author id to the user.id
-  const { loading, error, data } = useQuery(GET_MOVIES, {
-    variables: { where: { author: { id: { equals: currentUser.id } } } },
-  });
-
-  // If there is data or movies, call the hasSubmittedMovies function
-  useEffect(() => {
-    if (data) {
-      hasSubmittedMovie();
-    }
-  }, [data]);
-
-  // If the data length is great than 0, controll the movie state
-  const hasSubmittedMovie = () => {
-    if (data && data.movies.length > 0) {
-      setMovieSubmitted(true);
-    } else {
-      setMovieSubmitted(false);
-    }
-  };
-
-  return (
-    <div className="max-w-[1440px] relative m-auto">
-      {/* Conditionally render this Link if the user has submitted movies */}
-      {movieSubmitted && (
+  if (!isSubmitted) {
+    return (
+      <main className="mx-auto flex flex-col py-10">
+        <p className="text-center text-3xl font-bold uppercase pb-5">
+          SUBMIT A SLOP
+        </p>
         <Link
           className="xs:hidden block absolute right-0 mr-10 underline"
           to="/submitted-list"
         >
           View submitted
         </Link>
-      )}
-      <img src={titleImage} className="m-auto mt-10 mb-10"></img>
-      <Form
-        onSubmit={handleSubmit(handleFormSubmit)}
-        className={
-          submitted
-            ? `hidden`
-            : ` xs:w-[300px] sm:w-[453px] md:w-[453px] lg:w-[453px] xl:w-[453px] m-auto flex flex-col font-arialBold text-lg mb-10`
-        }
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="w-1/3 mx-auto flex flex-col gap-2"
+        >
+          <TextInput
+            label="Title *"
+            {...register("title", {
+              required: {
+                value: true,
+                message: "Title is required.",
+              },
+              minLength: {
+                value: 2,
+                message: "Title must be at least 2 characters.",
+              },
+              maxLength: {
+                value: 100,
+                message: "Title must be no more than 100 characters.",
+              },
+            })}
+          />
+          <FormError errors={errors} name="title" />
+          <TextArea
+            label="Description *"
+            {...register("description", {
+              required: {
+                value: true,
+                message: "Description is required.",
+              },
+              minLength: {
+                value: 10,
+                message: "Description must be at least 10 characters.",
+              },
+              maxLength: {
+                value: 1000,
+                message: "Description must be no more than 1000 characters.",
+              },
+            })}
+          />
+          <FormError errors={errors} name="description" />
+          <div className="flex gap-2">
+            <div className="">
+              <TextInput
+                label="Release Year"
+                type="number"
+                {...register("releaseYear", {
+                  minLength: {
+                    value: 4,
+                    message: "Release Year must be exactly 4 characters",
+                  },
+                  maxLength: {
+                    value: 4,
+                    message: "Release Year must be exactly 4 characters",
+                  },
+                })}
+              />
+              <FormError errors={errors} name="releaseYear" />
+            </div>
+            <div>
+              <TextInput
+                type="number"
+                label="Runtime"
+                {...register("runtime", {})}
+              />
+              <FormError errors={errors} name="runtime" />
+            </div>
+          </div>
+          <Dropzone
+            control={control}
+            label="Image"
+            name="image"
+            multiple={false}
+          />
+          <div className="flex gap-2">
+            <div className="">
+              <TextInput
+                label="Rotten Tomatoes Score"
+                type="number"
+                {...register("tomatoScore", {
+                  minLength: {
+                    value: 1,
+                    message:
+                      "Rotten Tomatoes Score must be at least 1 character",
+                  },
+                  maxLength: {
+                    value: 4,
+                    message:
+                      "Rotten Tomatoes Score must be at most 2 characters",
+                  },
+                })}
+              />
+              <FormError errors={errors} name="releaseYear" />
+            </div>
+            <div>
+              <TextInput label="How to Watch" {...register("howToWatch", {})} />
+              <FormError errors={errors} name="runtime" />
+            </div>
+          </div>
+          <Dropdown
+            label="Keywords *"
+            defaultValue={[]}
+            options={keywords}
+            name="keywords"
+            control={control}
+            isMulti
+          />
+          {!isValid ? (
+            <Button type="submit" disabled>
+              Yeah!
+            </Button>
+          ) : (
+            <Button type="submit">Yeah!</Button>
+          )}
+        </form>
+      </main>
+    );
+  }
+  return (
+    <main className="flex gap-[200px] flex-col m-auto max-w-[672px] my-[200px]">
+      <p className="font-arialRegular text-lg text-center">
+        Thanks for submitting a slop to our platform, dear goblin! Our team of
+        professional slop goblins will review your submission and publish it, if
+        your slop is actually sloppy, and doesn’t repeat movies already
+        published here
+      </p>
+      <button
+        onClick={() => setIsSubmitted(false)}
+        className="m-auto w-[453px] border-none font-bold font-arial text-lg/4 border py-4 px-4 bg-yellow"
       >
-        <Form.TextInput
-          value={title}
-          errors={errors}
-          register={register("title", {
-            required: "Title is required",
-          })}
-          name="title"
-          placeholder="Title"
-          labelText="Title *"
-          required
-          onChange={(e) => {
-            setValue("title", e.target.value, { shouldValidate: true });
-          }}
-          isValid={!errors.title}
-          classNameInput="bg-white"
-        />
-        {errors.title && <Form.Feedback message={errors.title.message} />}
-        <Form.TextArea
-          value={description}
-          errors={errors}
-          register={register("description", {
-            required: "Description is required",
-          })}
-          placeholder="Descritpion"
-          labelText="Description *"
-          onChange={(e) => {
-            setValue("description", e.target.value, { shouldValidate: true });
-          }}
-          isValid={!errors.description}
-          classNameTextArea="bg-white"
-        />
-        {errors.description && (
-          <Form.Feedback message={errors.description.message} />
-        )}
-        <div className="xs:flex-col sm:flex md:flex lg:flex xl:flex justify-between font-arialBold text-lg box-border ">
-          <div className="flex flex-col">
-            <Form.TextInput
-              required
-              maxLength={4}
-              value={releaseYear}
-              errors={errors}
-              register={register("releaseYear", {
-                required: "Release Year is required",
-                minLength: {
-                  value: 4,
-                  message: "Please enter 4 digits",
-                },
-              })}
-              placeholder="Release Year"
-              labelText="Release Year *"
-              onChange={(e) => {
-                if (!isNaN(e.target.value) && e.target.value !== " ") {
-                  setValue("releaseYear", e.target.value, {
-                    shouldValidate: true,
-                  });
-                }
-              }}
-              isValid={!errors.releaseYear}
-              classNameInput="bg-white"
-            />
-            {errors.releaseYear && (
-              <Form.Feedback message={errors.releaseYear.message} />
-            )}
-          </div>
-          <div className="flex flex-col">
-            <Form.TextInput
-              required
-              maxLength={3}
-              value={runtime}
-              errors={errors}
-              register={register("runtime", {
-                required: "Runtime is required",
-              })}
-              placeholder="Runtime"
-              labelText="Runtime *"
-              onChange={(e) => {
-                if (!isNaN(e.target.value) && e.target.value !== " ") {
-                  setValue("runtime", e.target.value, { shouldValidate: true });
-                }
-              }}
-              isValid={!errors.runtime}
-              classNameInput="bg-white"
-            />
-            {errors.runtime && (
-              <Form.Feedback message={errors.runtime.message} />
-            )}
-          </div>
-        </div>
-        {/* // For future use */}
-        {/* <Form.FileDrop
-          id="photo"
-          labelText="Image"
-          watch={watch}
-          setValue={setValue}
-          onChange={(file) => setValue("photo", file)}
-        >
-          Drop File here
-        </Form.FileDrop> */}
-        <div className="xs:flex-col sm:flex md:flex lg:flex xl:flex justify-between box-border font-arialBold text-lg">
-          <div className="flex flex-col">
-            <Form.TextInput
-              value={tomatoScore}
-              register={register("tomatoScore", {
-                pattern: {
-                  value: /^\d+%/,
-                  message: "Must be a percent",
-                },
-              })}
-              placeholder="Rotten Tomatoes Score"
-              labelText="Rotten Tomatoes Score"
-              onChange={(e) => {
-                setValue("tomatoScore", e.target.value, {
-                  shouldValidate: true,
-                });
-              }}
-              isValid={!errors.tomatoScore}
-              classNameInput="bg-white"
-            />
-            {errors.tomatoScore && (
-              <Form.Feedback message={errors.tomatoScore.message} />
-            )}
-          </div>
-          <div className="flex flex-col">
-            <Form.TextInput
-              value={howToWatch}
-              placeholder="How To Watch"
-              labelText="How To Watch"
-              onChange={(e) => {
-                setValue("howToWatch", e.target.value, {
-                  shouldValidate: true,
-                });
-              }}
-              isValid={!errors.howToWatch}
-              classNameInput="bg-white"
-            />
-          </div>
-        </div>
-        <Form.Combobox
-          list={movieKeywords}
-          watch={watch}
-          setValue={setValue}
-          labelText={"Keywords"}
-          placeholder={"Choose Keywords"}
-          id={"keywords"}
-          nameKey={"name"}
-          idKey={"name"}
-          name={"Keywords"}
-          classNameCombo="bg-white"
-        />
-        {errors.keywords && <Form.Feedback message={errors.keywords.message} />}
-        <Form.Submit
-          type="submit"
-          title="yeah"
-          className="w-full border-none"
-          disabled={!isValid}
-        />
-        <Link
-          className="xs:block hidden font-arialRegular m-auto mt-4 underline"
-          to="/submitted-slops"
-        >
-          View submitted
-        </Link>
-      </Form>
-      {/* If form is submitted, we are displaying a thank you message */}
-      {submitted ? (
-        <div className="flex gap-[200px] flex-col m-auto max-w-[672px] mt-[200px]">
-          <p className="font-arialRegular text-lg text-center">
-            Thanks for submitting a slop to our platform, dear goblin! Our team
-            of professional slop goblins will review your submission and publish
-            it, if your slop is actually sloppy, and doesn’t repeat movies
-            already published here
-          </p>
-          <button
-            onClick={resetSubmitted}
-            className="m-auto w-[453px] border-none font-bold font-arial text-lg/4 border py-4 px-4 bg-yellow"
-          >
-            Submit another one?
-          </button>
-        </div>
-      ) : (
-        ""
-      )}
-    </div>
+        Submit another one?
+      </button>
+    </main>
   );
-};
+}
