f3d8b612287e87980dbb6d1cc3f85bb8437bcc6d
Svetlana Kovakina <skovakina@gmail.com>
2024-06-04 08:06:43 -0700

    fix: fest page links

--- src/components/sidebar/sidebar-item.jsx
+++ src/components/sidebar/sidebar-item.jsx
@@ -8,7 +8,7 @@ export const SidebarItem = ({ item, className }) => {
       end
       className={({ isActive }) =>
         cx(
-          "flex gap-5 text-lg font-arialBold decoration-solid decoration-2 hover:underline hover:opacity-100",
+          "flex text-lg font-arialBold decoration-solid decoration-2 hover:underline hover:opacity-100",
           className,
           {
             "underline opacity-100 ": isActive,
@@ -17,7 +17,11 @@ export const SidebarItem = ({ item, className }) => {
         )
       }
     >
-      <img className="h-8 w-8 " src={item.src} alt={item.title} />
+      {item.img ? (
+        <img className="h-8 w-8 " src={item.src} alt={item.title} />
+      ) : (
+        <div className="w-5 "></div>
+      )}
       {item.title}
     </NavLink>
   );
