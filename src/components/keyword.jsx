9f46423d85b82194bc15925ce25bd9985b46a0ac
Jonathan <jonathan.hudson2012@gmail.com>
2024-07-17 20:41:39 -0400

    style: fix lint

--- src/components/keyword.jsx
+++ src/components/keyword.jsx
@@ -2,7 +2,7 @@ export const Keyword = ({ keyword, className }) => {
   return (
     <p
       className={`inline-flex items-center p-2 h-31 w-max text-base font-arial break-words leading-4 ${className}`}
-      data-test-id={ "keyword-id-" + keyword }
+      data-test-id={"keyword-id-" + keyword}
     >
       {keyword}
     </p>
