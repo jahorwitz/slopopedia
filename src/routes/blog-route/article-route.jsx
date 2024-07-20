7ec71651ae98c0701818031d0b76b719c8e9d928
Raviar1337 <132175474+Raviar1337@users.noreply.github.com>
2024-07-20 13:44:45 -0400

    Update src/routes/blog-route/article-route.jsx
    Co-authored-by: Jonathan Hudson <jonathan.hudson2012@gmail.com>

--- src/routes/blog-route/article-route.jsx
+++ src/routes/blog-route/article-route.jsx
@@ -6,9 +6,9 @@ export function ArticleRoute({ type }) {
     <>
       <div className="relative" data-test-id="article-header">
         <Header data-test-id="article-header">
-          <Header.Logo data-test-id="article-header-logo" />
-          <Header.NavLinks data-test-id="article-header-navlinks" />
-          <Header.Profile data-test-id="article-header-profile" />
+          <Header.Logo />
+          <Header.NavLinks />
+          <Header.Profile />
         </Header>
       </div>
       <ArticlePage type={type} data-test-id="article-articlePage" />
